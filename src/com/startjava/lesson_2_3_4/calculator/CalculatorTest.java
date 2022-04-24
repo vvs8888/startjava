dfe0084c8a30793c52f6e287127d046e095bdd82
vvs8888 <vvs8888@gmail.com>
2022-04-24 17:09:58 +0300

    Lesson 4 part 2.3

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -9,46 +9,40 @@ public class CalculatorTest {
             do {
                 boolean isCorrectInt = false;
                 int a = -1;
-                char oper = '~';
+                char operationSign = '~';
                 int b = -1;
                 do {
                     System.out.print("Введите математическое выражение через пробелы: ");
                     String str = scanner.nextLine();
-                    str = str.trim();
+                    str = str.replaceAll("\\s+", " ").trim();
                     try {
-                        if (str.length() < 5) throw new Exception("Введено менее 5 символов!");
-
-                        if (str.length() != str.replace(" ", "").length() + 2)
-                            throw new Exception("В выражении должно быть два разделителя пробела!");
-
-                        if (str.length() != str.replaceAll("\\s[+-/%^\\*]\\s", "").length() + 3)
-                            throw new Exception("Данный оператор не поддерживается!");
-
                         String[] words = str.split(" ");
                         for (String word : words) {
                             System.out.println(word);
                         }
-                        if (words.length != 3) throw new Exception("Некорректное число аргументов!");
+                        if (words.length != 3) throw new Exception("Некорректное число аргументов выражения!");
 
+                        if (!words[0].matches("[1-9]\\d*")) throw new Exception("Первый аргумент не целое положительное число!");
                         a = Integer.parseInt(words[0]);
-                        oper = words[1].charAt(0);
+
+                        if (!words[1].matches("[+-/%^*]")) throw new Exception("Оператор " + words[1] + " не поддерживается!");
+                        operationSign = words[1].charAt(0);
+
+                        if (!words[2].matches("[1-9]\\d*")) throw new Exception("Второй аргумент не целое положительное число!");
                         b = Integer.parseInt(words[2]);
 
-                        if (a <= 0 || b <= 0) throw new Exception("Необходимо ввести целые положительные числа!");
                         isCorrectInt = true;
-                    } catch (NumberFormatException ex) {
-                        System.out.println("Аргумент не целое число!");
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
                 } while (!isCorrectInt);
 
                 Calculator calcOne = new Calculator();
-                Result calcResult = calcOne.calc(a, oper, b);
+                Result calcResult = calcOne.calc(a, operationSign, b);
                 String errMsg = calcResult.getErrMsg();
 
                 if (errMsg == null) {
-                    System.out.printf("%d%s%d=%.2f%n", a, oper, b, calcResult.getResult());
+                    System.out.printf("%d%s%d=%.2f%n", a, operationSign, b, calcResult.getResult());
                 } else System.out.printf("%s%n", errMsg);
 
                 do {
